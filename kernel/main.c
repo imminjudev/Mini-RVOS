edfd00be596352f72d8a4124efb71f73649fbb9d
minju77 <imminju7@gmail.com>
2026-08-18 12:00:52 +0900

    feat: initialize bss during kernel boot

--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,17 @@
 void uart_puts(const char *s);
 
+volatile unsigned long bss_probe;
+
 void kernel_main(void)
 {
     uart_puts("Mini-RVOS booting...\n");
     uart_puts("Hello from kernel.\n");
+
+    if (bss_probe == 0) {
+        uart_puts("[OK] bss initialized\n");
+    } else {
+        uart_puts("[FAIL] bss initialization\n");
+    }
 
     for (;;) {
     }
